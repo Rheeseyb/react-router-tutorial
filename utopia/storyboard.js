0eb08f5d9be2aa6ba58e264001b4a4d0e3f4fc3f
RheeseyB <1044774+Rheeseyb@users.noreply.github.com>
2022-11-30 15:44:02 +0000

    With uids

--- utopia/storyboard.js
+++ utopia/storyboard.js
@@ -1,33 +1,85 @@
 import * as React from 'react'
 import { Scene, Storyboard } from 'utopia-api'
-import { App } from '/src/app.js'
-import { Playground } from '/src/playground.js'
 import {
   createMemoryRouter,
   createRoutesFromElements,
   RouterProvider,
   Route,
   Link,
+  Outlet,
 } from 'react-router-dom'
 
-const router = createMemoryRouter(
-  createRoutesFromElements(
-    <>
-      <Route
-        path='/'
-        element={<Link to={`contacts/1`}>Clicky</Link>}
-        errorElement={<div>404!</div>}
-      />
-      <Route
-        path='contacts/:id'
-        element={<div>Hello</div>}
-      />
-    </>,
-  ),
-)
+const Clicky = () => {
+  return (
+    <div
+      data-uid='clicky-root'
+      style={{ width: '100%', height: '100%' }}
+    >
+      <Link
+        to={`contacts/1`}
+        data-uid='link'
+        style={{ width: '100%', height: '100%' }}
+      >
+        Clicky
+      </Link>
+      <div data-uid='outlet-div'>
+        <Outlet data-uid='outlet' />
+      </div>
+    </div>
+  )
+}
+
+const Hello = () => {
+  return (
+    <div
+      style={{
+        width: '100',
+        height: '200',
+        backgroundColor: 'white',
+      }}
+      data-uid='hello-root'
+    >
+      Hello!
+    </div>
+  )
+}
+
+const Error404 = () => {
+  return <div data-uid='error-root'>404!</div>
+}
+
+const App = () => {
+  return (
+    <RouterProvider
+      router={createMemoryRouter(
+        createRoutesFromElements(
+          <Route
+            path='/'
+            element={<Clicky data-uid='clicky' />}
+            errorElement={
+              <Error404 data-uid='error-element' />
+            }
+            data-uid='route1'
+          >
+            <Route
+              path='contacts/:id'
+              element={<Hello />}
+              data-uid='route2'
+            />
+          </Route>,
+        ),
+        {
+          initialEntries: ['/', '/contacts/1'],
+          initialIndex: 1,
+        },
+      )}
+      data-uid='router-provider'
+    />
+  )
+}
 
 export var storyboard = (
-  <Storyboard>
+  <Storyboard data-uid='sb'>
     <Scene
       style={{
         width: 400,
@@ -37,8 +89,9 @@ export var storyboard = (
         top: 0,
       }}
       data-label='Playground'
+      data-uid='scene'
     >
-      <RouterProvider router={router} />
+      <App data-uid='app' />
     </Scene>
   </Storyboard>
 )
